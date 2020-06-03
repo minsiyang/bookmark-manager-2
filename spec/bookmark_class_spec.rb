1001ca0308de95d0bd2e0cad1c6fd1aafbff83f9
minsi yang <yangminsi@gmail.com>
2020-06-03 22:01:36 +0100

    Fixed test for bookmark_spec.rb and bookmark_class_spec.rb

--- spec/bookmark_class_spec.rb
+++ spec/bookmark_class_spec.rb
@@ -3,12 +3,17 @@ require 'bookmark'
 describe Bookmark do
 
   describe ".all" do
+    con = PG.connect :dbname => 'bookmark_manager_test'
+    
+    con.exec "INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');"
+    con.exec "INSERT INTO bookmarks (url) VALUES ('http://google.com');"
+    con.exec "INSERT INTO bookmarks (url) VALUES ('http://twitter.com');"
 
     list = Bookmark.all
 
     it "return a list of bookmarks" do
+      expect(list).to include("http://www.makersacademy.com")
       expect(list).to include("http://google.com")
-      expect(list).to include("http://facebook.com")
       expect(list).to include("http://twitter.com")
     end
   end
--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -1,10 +1,17 @@
 require 'sinatra'
 
 feature "/bookmarks page" do
-  xscenario "views bookmarks" do
+  scenario "views bookmarks" do
+    con = PG.connect :dbname => 'bookmark_manager_test'
+
+    con.exec "INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');"
+    con.exec "INSERT INTO bookmarks (url) VALUES ('http://google.com');"
+    con.exec "INSERT INTO bookmarks (url) VALUES ('http://twitter.com');"
+
     visit '/bookmarks'
+
     expect(page).to have_content("http://google.com")
-    expect(page).to have_content("http://facebook.com")
+    expect(page).to have_content("http://www.makersacademy.com")
     expect(page).to have_content("http://twitter.com")
   end
 end
