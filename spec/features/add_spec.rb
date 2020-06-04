a48a76c8d5dade914be4d054c833f0dd776294d4
minsi yang <yangminsi@gmail.com>
2020-06-04 13:18:01 +0100

    Changes on feature test for '/add'

--- spec/features/add_spec.rb
+++ spec/features/add_spec.rb
@@ -2,7 +2,7 @@ feature "/add" do
   scenario "see a form to fill in new url and submit" do
     visit "/add"
     fill_in("url", with:"http://amazingweb.com")
-    click("Save")
+    click_on("Save")
     expect(page).to have_content("http://amazingweb.com")
   end
 end
